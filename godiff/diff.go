6886b8da6b7d5a80096cbd61edcc404d15adbe71
Alex Kraasch <alex@kraasch.eu>
2025-02-02 23:34:39 +0100

    Add subtests and increase readability.

--- godiff/diff.go
+++ godiff/diff.go
@@ -8,17 +8,41 @@ import (
 )
 
 func Diff(a, b string) (out string) {
-  aa := strings.Split(a, "\n")
-  bb := strings.Split(b, "\n")
+
+  // temp variables.
+  switched := false
   outA := ""
   outB := ""
+
+  // split input by line.
+  aa := strings.Split(a, "\n")
+  bb := strings.Split(b, "\n")
+
+  // switch a and b so to loop over the longer one.
+  if len(bb) > len(aa) { // if bb is longer, swap them. ==> aa is always the longer one.
+    aa, bb = bb, aa
+    switched = true
+  }
+
+  // loop over aa (the longer one).
   for i := range aa {
-    if i > len(bb) || aa[i] != bb[i] {
-      outA += "-" + aa[i] + "\n"
-      outB += "+" + bb[i] + "\n"
+    if i > len(bb) {
+      outA += "|" + aa[i] + "\n"
+      continue
+    }
+    if aa[i] != bb[i] {
+      outA += "|" + aa[i] + "\n"
+      outB += ":" + bb[i] + "\n"
     }
   }
-  out = outA + outB
-  return 
+
+  // correct for having switched a and b.
+  if switched {
+    out = outB + outA
+  } else {
+    out = outA + outB
+  }
+
+  return
 }
 
--- godiff/diff_test.go
+++ godiff/diff_test.go
@@ -66,19 +66,25 @@ var suites = []TestSuite{
         testName:      "one-line+different-strings-00",
         inputA:        "",
         inputB:        "A",
-        expectedValue: "-\n+A\n",
+        expectedValue:
+                       "|\n" +
+                       ":A\n",
       },
       {
         testName:      "one-line+different-strings-01",
         inputA:        "A",
         inputB:        "",
-        expectedValue: "-A\n+\n",
+        expectedValue:
+                       "|A\n" +
+                       ":\n",
       },
       {
         testName:      "one-line+different-strings-02",
         inputA:        "A",
         inputB:        "B",
-        expectedValue: "-A\n+B\n",
+        expectedValue:
+                       "|A\n" +
+                       ":B\n",
       },
       {
         testName:      "multi-line+same-strings-00",
@@ -122,10 +128,10 @@ var suites = []TestSuite{
                        "a\n" +
                        "b\n",
         expectedValue:
-                       "-A\n" +
-                       "-B\n" +
-                       "+a\n" +
-                       "+b\n",
+                       "|A\n" +
+                       "|B\n" +
+                       ":a\n" +
+                       ":b\n",
       },
       {
         testName:      "multi-line+different-strings-01",
@@ -136,8 +142,8 @@ var suites = []TestSuite{
                        "A\n" +
                        "b\n",
         expectedValue:
-                       "-B\n" +
-                       "+b\n",
+                       "|B\n" +
+                       ":b\n",
       },
       {
         testName:      "multi-line+different-strings-02",
@@ -148,9 +154,53 @@ var suites = []TestSuite{
                        "A\n" +
                        "b\n",
         expectedValue:
-                       "-\n" +
-                       "+b\n",
+                       "|\n" +
+                       ":b\n",
       },
+      {
+        testName:      "more-complicated-00",
+        inputA:
+                       "Mo Tu We Th Fr Sa Su" + NL +
+                       "       1  2  3  4  5" + NL,
+        inputB:
+                       "Mo Tu We Th Fr Sa Su" + NL +
+                       "                1  2" + NL,
+        expectedValue:
+                       "|       1  2  3  4  5" + NL +
+                       ":                1  2" + NL,
+      },
+      // {
+      //   testName:      "more-empty-lines-in-b-than-a-00",
+      //   inputA:
+      //                  "Mo Tu We Th Fr Sa Su" + NL +
+      //                  "       1  2  3  4  5" + NL +
+      //                  " 6  7  8  9 10 11 12" + NL +
+      //                  "13 14 15 16 17 18 19" + NL +
+      //                  "20 21 22 23 24 25 26" + NL +
+      //                  "27 28"                + NL +
+      //                  ""                     + NL,
+      //   inputB:
+      //                  "Mo Tu We Th Fr Sa Su" + NL +
+      //                  "                1  2" + NL +
+      //                  " 3  4  5  6  7  8  9" + NL +
+      //                  "10 11 12 13 14 15 16" + NL +
+      //                  "17 18 19 20 21 22 23" + NL +
+      //                  "24 25 26 27 28"       + NL +
+      //                  ""                     + NL +
+      //                  ""                     + NL,
+      //   expectedValue:
+      //                  "|       1  2  3  4  5" + NL +
+      //                  "| 6  7  8  9 10 11 12" + NL +
+      //                  "|13 14 15 16 17 18 19" + NL +
+      //                  "|20 21 22 23 24 25 26" + NL +
+      //                  "|27 28"                + NL +
+      //                  ":                1  2" + NL +
+      //                  ": 3  4  5  6  7  8  9" + NL +
+      //                  ":10 11 12 13 14 15 16" + NL +
+      //                  ":17 18 19 20 21 22 23" + NL +
+      //                  ":24 25 26 27 28"       + NL +
+      //                  ":"                     + NL,
+      // },
     },
   },
 }
@@ -159,11 +209,14 @@ func TestAll(t *testing.T) {
   for _, suite := range suites {
     for _, test := range suite.tests {
       name := test.testName
-      exp := test.expectedValue
-      got := suite.functionUnderTest(test.inputA, test.inputB)
-      if exp != got {
-        t.Errorf("In '%s':\n  Exp: '%#v'\n  Got: '%#v'\n", name, exp, got)
-      }
+      t.Run(name, func(t *testing.T) {
+        exp := test.expectedValue
+        got := suite.functionUnderTest(test.inputA, test.inputB)
+        // if !reflect.DeepEqual(tc.want, got) { // TODO: implement.
+        if exp != got {
+          t.Errorf("In '%s':\n  Exp: '%#v'\n  Got: '%#v'\n", name, exp, got)
+        }
+      })
     }
   }
 }
